85370f2303ef85de3a00d9fffa1df326d88d9d53
mardoqueiro <mordecai.dimbu@gmail.com>
2026-08-08 20:34:30 +0200

    lesson1

---
+++ unit_01/lesson.py
@@ -0,0 +1,11 @@
+# Making the computer speak
+print("Hello World!")
+
+# Storing data inside a variable
+name = "Mordecai"
+print(name)
+print("name")
+
+# Making our code interactive
+name = input("Enter your name: ")
+print(f"Greetings, {name}")
